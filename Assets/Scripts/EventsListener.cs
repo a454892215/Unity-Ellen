dacf7804de9b2b518a6a5e431b41a5c9bd1d940b
L <unkown@.com>
2020-01-06 18:32:13 +0800

    首页搭建

---
+++ Assets/Scripts/EventsListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject target;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    RectTransform indexTrans;
+    void Start()
+    {
+        indexTrans = transform.Find("Index").GetComponent<RectTransform>();
+        indexTrans.DORotate(new Vector3(0, 0, -180), 1).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+        indexTrans.DOPause();
+        indexTrans.gameObject.SetActive(false);
+
+    }
+
+    public void StartRotate()
+    {
+        indexTrans.gameObject.SetActive(true);
+        indexTrans.DORestart();
+    }
+
+    public void PauseRotate()
+    {
+        indexTrans.gameObject.SetActive(false);
+        indexTrans.DOPause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
